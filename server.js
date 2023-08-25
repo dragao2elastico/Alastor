8b783c265d2d166c087e780ff1ce5926d9f1f766
dragao-elastico <arthur.santiago.silva@escola.pr.gov.br>
2023-08-25 18:39:49 -0300

    .

--- server.js
+++ server.js
@@ -8,12 +8,12 @@ var port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods','GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-});
+// app.use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     res.setHeader('Access-Control-Allow-Methods','GET, POST, PUT, DELETE');
+//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//     next();
+// });
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
@@ -27,12 +27,44 @@ app.get('/api', (req, res) => {
 });
 
 app.get('/:ip', (req, res) => {
+    // res.send(`Ip Grabber\n`);
     const ip = req.params.ip;
     const htmlContent = `
-        <script data-ip="${ip}" src="/ip.js"></script>
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>/Ip</title>
+            <style></style>
+            <link rel="stylesheet" href="/style/global.css">
+        </head>
+        <body>
+            <div class="spoiler-container" onclick="toggleSpoiler(this)">
+                Click to show
+                <div class="spoiler-content" id="ip">
+                    Hello, this is your IP address: ${req.params.ip}
+                </div>
+            </div>
+
+            <script>
+                function toggleSpoiler(spoiler) {
+                    spoiler.classList.toggle("show");
+                }
+            </script>
+        </body>
+        </html>
     `;
+    
     res.send(htmlContent);
-    res.sendFile("./server/views/ip.html")
+});
+
+app.get('/scripts/ip.js', (req, res) => {
+    res.sendFile(path.join(__dirname, 'ip.js'));
+});
+
+app.get('/style/global.css', (req, res) => {
+    res.sendFile(path.join(__dirname, 'server', 'style', 'global.css'));
 });
 
 app.listen(port, () => {
