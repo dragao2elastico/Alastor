d12d758fceadae6745042356a17ac8b6100fc6fa
dragao-elastico <arthur.santiago.silva@escola.pr.gov.br>
2023-08-26 09:57:21 -0300

    .

--- server.js
+++ server.js
@@ -2,10 +2,15 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 const axios = require("axios");
+const prompt = require("prompt-sync")();
 require("colors");
 
+var askPort = false;
+
+if (askPort === true) port = prompt("Which port you want i create? ")
+else if (askPort === false) port = 3000
+
 const app = express();
-const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -29,39 +34,12 @@ app.get('/api', (req, res) => {
 
 app.get('/:ip', async (req, res) => {
     try {
-        const clientIP = req.params.ip;
-
         const response = await axios.get("https://api.ipify.org?format=json");
         const publicIP = response.data.ip;
 
-        const htmlContent = `
-            <!DOCTYPE html>
-            <html lang="en">
-            <head>
-                <meta charset="UTF-8">
-                <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <title>/Ip</title>
-                <link rel="stylesheet" href="/style/global.css">
-            </head>
-            <body>
-                <div class="spoiler-container" onclick="toggleSpoiler(this)">
-                    Click to show
-                    <div class="spoiler-content" id="ip">
-                        <hr>
-                        Hello, this is your IP address: ${publicIP}
-                    </div>
-                </div>
-
-                <script>
-                    function toggleSpoiler(spoiler) {
-                        spoiler.classList.toggle("show");
-                    }
-                </script>
-            </body>
-            </html>
-        `;
-
-        res.send(htmlContent);
+        res.sendFile(path.join(__dirname, 'ip.html'), {
+            ip: publicIP // Variável que será inserida no HTML
+        });
     } catch (error) {
         console.error("Error fetching IP: ", error);
         res.status(500).send("Error fetching IP");
@@ -72,5 +50,5 @@ app.use('/scripts', express.static(path.join(__dirname, 'scripts')));
 app.use('/style', express.static(path.join(__dirname, 'server', 'style')));
 
 app.listen(port, () => {
-    console.log(`Server running on`, `http://localhost:${port}`.blue);
+    console.log(`Server running on:`, `http://localhost:${port}`.blue);
 });
