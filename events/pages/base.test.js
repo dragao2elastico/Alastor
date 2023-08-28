ca844ed35da3a839145432a0805884010781c790
dragao-elastico <arthur.santiago.silva@escola.pr.gov.br>
2023-08-28 16:51:58 -0300

    Modified files and folders

--- events/pages/base.test.js
+++ events/pages/base.test.js
@@ -1,12 +1,15 @@
+const path = require('path');
 require("colors");
 
 module.exports = {
     name: "", 
     handler: (req, res, next) => {
         try {
+            const filePath = path.join(__dirname, `../../server/views/${this.name}`);
+            res.sendFile(filePath);
         } catch (error) {
-            console.error("An Error Occurred: ".red, error);
-            res.status(500).send("An error ocurred: " + error.message);
+            console.error('An Error Occurred: '.red, error);
+            res.status(500).send('An error ocurred: ' + error.message);
         }
     }
 };
